75277f78c27966274967278f5fc7c86605489c60
kuubs23 <101410378+kuubs23@users.noreply.github.com>
2023-07-04 22:56:44 +0200

    extra files added

--- panels/tests/tests_shell/test_lb_orthotropic.py
+++ panels/tests/tests_shell/test_lb_orthotropic.py
@@ -1,6 +1,7 @@
 import numpy as np
 from structsolve import lb
 import matplotlib
+from structsolve import solve
 
 from panels import Shell
 from panels.plot_shell import plot_shell
@@ -12,21 +13,26 @@ def test_lb_orthotropic():
     s.m = 12
     s.n = 12
     s.stack = [0, 90, -45, +45, +45, -45, 90, 0]
-    s.plyt = 0.125e-3
-    s.laminaprop = (142.5e9, 8.7e9, 0.28, 5.1e9, 5.1e9, 5.1e9)
+    s.plyt = 0.125
+    s.laminaprop = (142.5e3, 8.7e3, 0.28, 5.1e3, 5.1e3, 5.1e3)
     s.model = 'cylshell_clpt_donnell_bardell'
-    s.a = 2.
-    s.b = 1.
+    s.a = 500
+    s.b = 250
     # radius
-    s.r = 2.
+    s.r = 1.e15	
 
     # compression
     s.Nxx = -1
     # shear
-    s.Nxy = -1
+    s.Nxy = 0
 
-    eigvals, eigvecs = lb(s.calc_kC(), s.calc_kG(), silent=True)
-    plot_shell(s, eigvecs[:, 1], vec='w')
+    # fext = s.calc_fext()
+    # print('  fext', fext)
+
+    eigvals, eigvecs = lb(s.calc_kC(), s.calc_kG(), silent=False)
+    plot_shell(s, eigvecs[:, 0], vec='w')
+
+    print('  linear wmax', s.fields['w'].max())
 
 if __name__ == '__main__':
     test_lb_orthotropic()
